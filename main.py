368e7015942a08e007fb9587f73cd7ea0c33c3ea
OM provisioning <jordi.febrer@gmail.com>
2020-04-29 13:11:33 +0200

    deleted vulnerabilities

--- main.py
+++ main.py
@@ -2,42 +2,42 @@ import yaml
 
 print("This is a test.")
 
-# dummy comment
+# # dummy comment
 
-with open("example.yaml", 'r') as stream:
-    try:
-        # waiting for sonarqube
-        print(yaml.load(stream))
-        # print(yaml.safe_load(stream))
-    except yaml.YAMLError as exc:
-        print(exc)
+# with open("example.yaml", 'r') as stream:
+#     try:
+#         # waiting for sonarqube
+#         print(yaml.load(stream))
+#         # print(yaml.safe_load(stream))
+#     except yaml.YAMLError as exc:
+#         print(exc)
 
-# code smells
+# # code smells
 
-DATA = ['a', 'b', 'c']
+# DATA = ['a', 'b', 'c']
 
-for i in range(len(DATA)):
-    print(DATA[i])
+# for i in range(len(DATA)):
+#     print(DATA[i])
 
-def foo(a):  # NonCompliant
-    b = 12
-    if a == 1:
-        return b
-    return b
+# def foo(a):  # NonCompliant
+#     b = 12
+#     if a == 1:
+#         return b
+#     return b
 
-# bugs
+# # bugs
 
-class MyClass:
-    def instance_method():  # Noncompliant. "self" parameter is missing.
-        print("instance_method")
+# class MyClass:
+#     def instance_method():  # Noncompliant. "self" parameter is missing.
+#         print("instance_method")
 
-    @classmethod
-    def class_method():  # Noncompliant. "cls" parameter is missing.
-        print("class_method")
+#     @classmethod
+#     def class_method():  # Noncompliant. "cls" parameter is missing.
+#         print("class_method")
 
-# vulnerabilities
+# # vulnerabilities
 
-import tempfile
+# import tempfile
 
-filename = tempfile.mktemp() # Noncompliant
-tmp_file = open(filename, "w+")
+# filename = tempfile.mktemp() # Noncompliant
+# tmp_file = open(filename, "w+")
