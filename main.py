ca92b3efb82fa9a90dfd748ce794014ea9be2cc5
OM provisioning <jordi.febrer@gmail.com>
2020-04-29 11:56:35 +0200

    Adding code smells for testing purposes

--- main.py
+++ main.py
@@ -11,3 +11,16 @@ with open("example.yaml", 'r') as stream:
         # print(yaml.safe_load(stream))
     except yaml.YAMLError as exc:
         print(exc)
+
+# code smells
+
+DATA = ['a', 'b', 'c']
+
+for i in range(len(DATA)):
+    print(DATA[i])
+
+def foo(a):  # NonCompliant
+    b = 12
+    if a == 1:
+        return b
+    return b
