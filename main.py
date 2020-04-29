2d2824368b92dc5a4e96a9c3f938efe8c804c0d5
OM provisioning <jordi.febrer@gmail.com>
2020-04-29 11:43:08 +0200

    Adding a possible vulnerability

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
+import yaml
+
 print("This is a test.")
+
+# dummy comment
+
+with open("example.yaml", 'r') as stream:
+    try:
+        # waiting for sonarqube
+        print(yaml.load(stream))
+        # print(yaml.safe_load(stream))
+    except yaml.YAMLError as exc:
+        print(exc)
